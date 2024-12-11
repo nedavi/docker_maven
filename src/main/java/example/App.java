4f52c23900ecca5d15a8120f14c48922bbe4abf1
David S <negeorgefloyd@gmail.com>
2024-12-11 13:50:53 +0300

    firsssssst

--- src/main/java/example/App.java
+++ src/main/java/example/App.java
@@ -2,6 +2,6 @@ package example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, Docker with Maven!");
+        System.out.println("Hello, esjkere!");
     }
 }
